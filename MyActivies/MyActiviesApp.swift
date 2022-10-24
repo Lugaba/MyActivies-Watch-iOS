2c257665d09a31c7a33882d8b01c30a92fda3b79
Luca Hummel <luca.hummel12@gmail.com>
2022-10-24 16:16:09 -0300

    Initial Commit

---
+++ MyActivies/MyActiviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyActiviesApp.swift
+//  MyActivies
+//
+//  Created by Luca Hummel on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MyActiviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
